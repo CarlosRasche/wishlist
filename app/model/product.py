bac93d79ad8fcd92f76380562bbfb6ee442b0b39
IgnacioRV <nacho.rasche@gmail.com>
2020-03-21 15:34:11 +0100

    Add Product and ProductList classes

---
+++ app/model/product.py
@@ -0,0 +1,9 @@
+class Product:
+    def __init__ (self, name: str, score: int):
+        self.name = name
+        self.score = score
+
+
+
+if __name__ == '__main__':
+    print("ninja")
---
+++ app/model/product_list.py
@@ -0,0 +1,21 @@
+from product import Product
+from typing import List
+
+class ProductList:
+    def __init__ (self, name: str, product_list: List[Product] = None):
+        self.name = name
+        self.product_list = product_list if product_list is not None else []
+    
+    def add_product (self, product_name: str, product_score: int) -> None:
+        prod1 = Product(name = product_name, score = product_score)
+        self.product_list.append(prod1)
+        
+
+
+if __name__ == '__main__':
+    print("hola")
+    plist = ProductList(name = "pl1")
+    print (plist.name)
+    plist.add_product(product_name = "firstprod", product_score = 8)
+    plist.add_product(product_name = "guac", product_score = 4)
+    print([f"{product.name} --- {product.score}"  for product in plist.product_list])
