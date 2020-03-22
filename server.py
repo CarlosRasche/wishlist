f0c6ea52b39d6319d88c7c71f74ff89de547832a
CarlosRasche <carlosrv99@gmail.com>
2020-03-22 15:04:09 +0100

    Init product list on startup

--- server.py
+++ server.py
@@ -1,15 +1,21 @@
 from aiohttp import web
+from app.model.product_list import ProductList
 
 
 async def handle(request):
     name = request.match_info.get("name", "Anonymous")
-    text = "Hello, " + name
+    text = f'''Hello, this is the list {request.app.product_list._name}
+{request.app.product_list._product_list}'''
+        
     return web.Response(text=text)
 
 
 app = web.Application()
 app.add_routes([web.get("/", handle), web.get("/{name}", handle)])
+app.product_list = ProductList(name="wishlist")
 
 
 if __name__ == "__main__":
+    print(app.product_list._name)
     web.run_app(app)
+    print("app.name")
